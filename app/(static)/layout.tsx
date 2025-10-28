40c70733fe217bf651dbf3fd42a1c1a386da7285
kunalkeshan <kunalkeshan12@gmail.com>
2025-10-29 00:10:43 +0530

    feat: Add new UI components and update dependencies

--- app/(static)/layout.tsx
+++ app/(static)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -10,5 +11,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      <Toaster richColors />
+    </>
+  );
 }
